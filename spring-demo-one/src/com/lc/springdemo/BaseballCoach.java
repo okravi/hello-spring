e1e75c8b7e4f90b31d22f73ce332fd2f95074856
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-07-13 15:08:52 +0300

    initial commit

---
+++ spring-demo-one/src/com/lc/springdemo/BaseballCoach.java
@@ -0,0 +1,11 @@
+package com.lc.springdemo;
+
+public class BaseballCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		return "Spend 30 min playing baseball";
+	}
+	
+	
+}
---
+++ spring-demo-one/src/com/lc/springdemo/TrackCoach.java
@@ -0,0 +1,10 @@
+package com.lc.springdemo;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Run 5k";
+	}
+
+}
