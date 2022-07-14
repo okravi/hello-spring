f2af531e14c5ad72e3366c18ccd82a5c610d8ac1
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-07-14 16:45:01 +0300

    progress update

--- src/com/lc/springdemo/TrackCoach.java
+++ src/com/lc/springdemo/TrackCoach.java
@@ -1,10 +1,25 @@
 package com.lc.springdemo;
 
 public class TrackCoach implements Coach {
+	
+	private FortuneService fortuneService;
+
+	
+public TrackCoach(FortuneService theFortuneService) {
+		
+		this.fortuneService = theFortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		return "Run 5k";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		return fortuneService.getFortune();
+
 	}
 
 }
